2e069e4ad71bc3e2181e914513f53a879fa1fa8e
Vlad78 <dssdom@gmail.com>
2023-03-11 22:46:13 +0300

    feat: menu, header were adapted for phones

--- js/main.js
+++ js/main.js
@@ -8,3 +8,24 @@
     }
   };
 })();
+
+(function () {
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".header__nav");
+  const close = document.querySelector(".close");
+  const overlay = document.querySelector(".header__overlay");
+  burger.addEventListener("click", () => {
+    menu.classList.add("header__nav_active");
+    overlay.classList.add("header__overlay_active");
+  });
+
+  close.addEventListener("click", () => {
+    menu.classList.remove("header__nav_active");
+    overlay.classList.remove("header__overlay_active");
+  });
+
+  overlay.addEventListener("click", () => {
+    menu.classList.remove("header__nav_active");
+    overlay.classList.remove("header__overlay_active");
+  });
+})();
